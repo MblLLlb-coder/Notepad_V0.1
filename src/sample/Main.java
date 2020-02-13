d49a6cb9ae34cb53a7e704744f511476b6992433
Мышь <holodok223114@gmail.com>
2020-02-14 00:09:52 +0300

    Performance improvement a little

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;//
@@ -31,17 +32,17 @@ public class Main extends Application {
         bMusic.setLayoutX(225);
         bMusic.setLayoutY(0);
         bMusic.setOnAction(actionEvent -> {
-                    try {
-                        File sf = new File("E:/Загрузки/*NameOfMusic*.wav");//Path to music you need(.wav is required)
-                        AudioInputStream ais = AudioSystem.getAudioInputStream(sf);
-                        Clip clip = AudioSystem.getClip();
-                        clip.open(ais);
-                        clip.setFramePosition(0);
-                        clip.start();
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
-                    }
-                });
+            try {
+                File sf = new File("E:/Загрузки/Arthas.wav");//Path to music you need(.wav is required)
+                AudioInputStream ais = AudioSystem.getAudioInputStream(sf);
+                Clip clip = AudioSystem.getClip();
+                clip.open(ais);
+                clip.setFramePosition(0);
+                clip.start();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        });
         TextField name = new TextField();
         name.setLayoutY(30);
         name.setLayoutX(0);
@@ -54,13 +55,15 @@ public class Main extends Application {
         bName.setVisible(false);
         Label description = new Label();
         description.setText("1-create the note\n2-Put some text in your note,\n press the save button and name the note \n(OK will save your note)");
-description.setLayoutX(5);
-description.setLayoutY(250);
+        description.setLayoutX(5);
+        description.setLayoutY(250);
 
         bCreate.setOnAction(actionEvent -> {
             note.setVisible(true);
             note.setMaxSize(600, 200);
             note.setLayoutY(65);//YY
+            String st = note.getText();
+            note.deleteText(0, st.length());
 
         });
         bSave.setOnAction(actionEvent -> {
@@ -68,13 +71,14 @@ description.setLayoutY(250);
             bName.setVisible(true);
             name.setVisible(true);
 
+
             bName.setOnAction(actionEvent1 -> {
                 String NameofNote = name.getText();
                 if (NameofNote == null) {
                     System.exit(54);
                 }
 
-                try (FileWriter fw = new FileWriter("C:/Users/Мышь/Desktop/" + NameofNote.trim() + ".npz")) {//you can replace .npz with simple .txt
+                try (FileWriter fw = new FileWriter("C:/Users/Мышь/Desktop/" + NameofNote.trim() + ".txt")) {//you can replace .npz with simple .txt
 
                     fw.write(s);
                     fw.append('\n');
@@ -84,8 +88,8 @@ description.setLayoutY(250);
                 } catch (Exception ex) {
                     ex.printStackTrace();
                 }
-                String s2 = " ";
-note.deleteText(0,s.length());
+                //String s2 = " ";
+                note.deleteText(0, s.length());
 
             });
 
@@ -113,14 +117,14 @@ note.deleteText(0,s.length());
                 nameOffile.setLayoutY(30);
 
 
-                try (FileReader reader = new FileReader("C:/Users/Мышь/Desktop/" + name1.trim() + ".npz")) {//you can replace .np for simple .txt
+                try (FileReader reader = new FileReader("C:/Users/Мышь/Desktop/" + name1.trim() + ".txt")) {//you can replace .np for simple .txt
                     Scanner sc = new Scanner(reader);
                     String st = note.getText();
                     while (sc.hasNextLine()) {
                         String s = sc.nextLine();
                         note.appendText(s);
 
-                        note.deleteText(0,st.length());
+                        note.deleteText(0, st.length());
 
                     }
 
@@ -130,7 +134,7 @@ note.deleteText(0,s.length());
 
             });
         });
-        Group gr = new Group(bCreate, bOpen, bSave, note, bName, name,description, bMusic);
+        Group gr = new Group(bCreate, bOpen, bSave, note, bName, name, description, bMusic);
 
 
         primaryStage.setScene(new Scene(gr, 600, 400));
